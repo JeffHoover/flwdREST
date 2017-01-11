454860aa87232e22772aadd61e8bb01aa093f0a5
Jeff Hoover <jhoover@jhoover.com>
2017-01-11 00:38:06 +0000

    README, more advanced call of py from c

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ http://192.168.1.197:4437/flwd/test
 When you do, the console on the server will say:
     Set this word on the FLWD: TEST
 
+-----
+g++ $(python-config --includes --libs) call_py.c -lpython2.7 -o call_py
+./call_py
+output will be:
+   hello
+
+g++ $(python-config --includes --libs) call_ext_py.c -lpython2.7 -o call_ext_py
+
+
---
+++ call_ext_py.c
@@ -0,0 +1,49 @@
+#include <Python.h>
+#include <pythonrun.h>
+
+int main(int argc, char *argv[])
+{
+    PyObject *pName, *pModule, *pDict, *pFunc, *pValue;
+
+    if (argc < 3) 
+    {
+        printf("Usage: exe_name python_source function_name\n");
+        return 1;
+    }
+
+    // Initialize the Python Interpreter
+    Py_Initialize();
+
+    PyObject *sys = PyImport_ImportModule("sys");
+    PyObject *path = PyObject_GetAttrString(sys, "path");
+    PyList_Append(path, PyString_FromString("."));
+
+    // Build the name object
+    pName = PyString_FromString(argv[1]);
+
+    // Load the module object
+    pModule = PyImport_Import(pName);
+
+    // pDict is a borrowed reference 
+    pDict = PyModule_GetDict(pModule);
+
+    // pFunc is also a borrowed reference 
+    pFunc = PyDict_GetItemString(pDict, argv[2]);
+
+    if (PyCallable_Check(pFunc)) 
+    {
+        PyObject_CallObject(pFunc, NULL);
+    } else 
+    {
+        PyErr_Print();
+    }
+
+    // Clean up
+    Py_DECREF(pModule);
+    Py_DECREF(pName);
+
+    // Finish the Python Interpreter
+    Py_Finalize();
+
+    return 0;
+}
